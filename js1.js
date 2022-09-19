2b71e42d901b037ce314363ea5e77e8075e35e29
melisaapoggilicenciada <112989336+melisaapoggilicenciada@users.noreply.github.com>
2022-09-18 21:50:49 -0300

    Add files via upload

---
+++ js1.js
@@ -0,0 +1,13 @@
+let edad1 = prompt("Introduce tu edad");
+let sexo = prompt("Introduce el sexo M/F:");
+if ((edad1 >= 20) && (sexo == "M")) {
+    alert("Felicitaciones puedes ingresar!!")
+    
+} else
+    if ((edad1 >=18) && (sexo=="H")){
+    alert ("Felicitaciones puedes ingresar!!")
+} else {
+    alert ("Prohibido el ingreso")
+}{
+    
+}
